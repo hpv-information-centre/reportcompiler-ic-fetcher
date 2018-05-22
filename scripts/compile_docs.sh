bce1607550c566eb109f9607a40d5b84b0c3c092
David Gomez <dgomez@iconcologia.net>
2018-05-22 13:48:35 +0200

    Fix scripts

--- scripts/compile_docs.sh
+++ scripts/compile_docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd ../doc
+BASE_DIR=`dirname $(readlink -f $0)`/..
+cd $BASE_DIR/doc
 make html
--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir ../build
-python ../setup.py sdist
-pip install -U --user -b ../build ../dist/*
-rm -R ../dist
-rm -R ../*.egg-info
-rm -R ../build
+BASE_DIR=`dirname $(readlink -f $0)`/..
+cd $BASE_DIR/doc
+mkdir $BASE_DIR/build
+python $BASE_DIR/setup.py sdist
+pip install -U --user -b $BASE_DIR/build $BASE_DIR/dist/*
+rm -R $BASE_DIR/dist
+rm -R $BASE_DIR/*.egg-info
+rm -R $BASE_DIR/build
